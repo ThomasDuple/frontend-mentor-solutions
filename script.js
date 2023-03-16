4a6b51733be150a2c41ca4a06c6e2052f26836a9
ThomasDuple <thomasduple@gmail.com>
2023-03-17 00:38:47 +0100

    fix links order

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ shelfElements.forEach(elem => {
     card.querySelector("img").src = elem.pic;
     card.querySelector("h1").textContent = elem.title;
     const a = card.querySelectorAll("a");
-    a[0].href = elem.linkChallenge;
-    a[1].href = elem.linkSolution;
+    a[0].href = elem.linkSolution;
+    a[1].href = elem.linkChallenge;
 
     shelf.appendChild(card);
 })
